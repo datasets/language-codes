c1a2f1e218c4b42a078907a1b8fcd86a4118c9ed
David Hermanns <okfn@hirntodt.de>
2015-01-15 21:22:49 +0100

    changed fiel description

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo '"alpha3 b","alpha3 t","alpha2","english","french"' > language-codes.csv
+echo '"alpha3-b","alpha3-t","alpha2","english","french"' > language-codes.csv
 curl http://www.loc.gov/standards/iso639-2/ISO-639-2_utf-8.txt | sed 's/|/","/g' | awk '{print "\"" $0"\""}' >> language-codes.csv
 
 
