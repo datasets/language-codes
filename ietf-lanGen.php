da6eabf7574d5f2965b6852bae68e13d5f2d11a8
ppkrauss <ppkrauss@gmail.com>
2015-08-31 07:33:59 +0000

    dftLang column in ietf-language-tags for issue #5 of datasets

--- ietf-lanGen.php
+++ ietf-lanGen.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * Extracts all lang codes and basic metadata of dir.
- * Folder $dir obtained from http://www.unicode.org/Public/cldr/latest/core.zip
+ * Extracts all lang codes and basic metadata.
+ * @version 2015-08-31
+ * @usage php ietf-lanGen.php > data/ietf-language-tags.csv
  */
 
-$dir= 'main';
+//CONFIGS:
+	$dir= 'main'; // "common/main" obtained from http://www.unicode.org/Public/cldr/latest/core.zip
+	$cldrCore_js = 'https://raw.githubusercontent.com/unicode-cldr/cldr-core/master/defaultContent.json';
+	// see https://github.com/unicode-cldr/cldr-core
+
+
+$cldrCore    = json_decode( file_get_contents($cldrCore_js), TRUE );
+$cldrCoreDft = array_map( 'strtolower', $cldrCore['defaultContent'] );
 
 $dom = new DOMDocument;
 $n=0;
-print "lang,langType,territory,revGenDate,defs,file";
+print "lang,langType,territory,revGenDate,defs,dftLang,file";
 foreach(scandir($dir) as $file) if (preg_match('/^(.+)\.xml$/',$file,$m)) {
-    $lang = strtr($m[1],'_','-');
+    $lang = $m[1];
     $dom->load("$dir/$file");
     $xp = new DOMXpath($dom);
     $revGenDate = $xp->evaluate("string(/ldml/identity/generation/@date)");
@@ -18,8 +26,10 @@ foreach(scandir($dir) as $file) if (preg_match('/^(.+)\.xml$/',$file,$m)) {
     $territory  = $xp->evaluate("string(/ldml/identity/territory/@type)");
     $defs       = $xp->evaluate("count(/ldml/*[not(self::identity)])");
     $revGenDate = preg_replace('/^[^\d]+(\d+\-\d+\-\d+).+$/','$1',$revGenDate);
-    print "\n$lang,$langType,$territory,$revGenDate,$defs,$file";
+    $isDftLang  = in_array(strtolower($lang),$cldrCoreDft)? '1': '0'; // yes or not
+    $lang = strtr($lang,'_','-');
+    print "\n$lang,$langType,$territory,$revGenDate,$defs,$isDftLang,$file";
     $n++;
 }
-//print "\n--- END: $n lang codes ---\n";
+// print "\n--- END: $n lang codes ---\n";
 ?>
